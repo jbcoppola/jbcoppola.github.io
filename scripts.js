583d1776365e0e473d8eca0ea4935c0e689b5152
Joseph Coppola <jbcoppola@gmail.com>
2017-02-26 17:40:01 -0500

    added javascript for button scrolling

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-﻿
+﻿$(document).ready(function () {
+
+    //when the nav button is clicked, it scrolls to that section
+    $(".btn-section-nav").on("click", function (event) {
+
+        //stops the href jump from occuring when javascript runs
+        event.preventDefault();
+
+        //gets the href location and scrolls to it
+        $("body, html").animate({
+            scrollTop: $($(this).attr('href')).offset().top},
+            "slow");
+        })
+});
