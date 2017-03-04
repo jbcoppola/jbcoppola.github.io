91cc4f8e1dc0bf68fbf1746c74d1258e638e7aa3
Joseph Coppola <jbcoppola@gmail.com>
2017-03-04 14:07:32 -0500

    changed header image and added hover border for project buttons

--- scripts.js
+++ scripts.js
@@ -13,23 +13,51 @@
             "slow");
     })
     //functions for replacing the project image with the full example picture
+    //white-background doesn't seem to currently do anything
     $("#siegenut").on("click", function () {
         //fade out, replace source, fade in
+        $($(".projectButtons").children("img")).removeClass("white-background");
+        $("#siegenut").addClass("white-background");
         $("#project-display").fadeOut(400, function () {
             $("#project-display").attr("src", "images/siegenut-full.png");
         })
         .fadeIn(400);
     });
     $("#tuscany").on("click", function () {
+        $($(".projectButtons").children("img")).removeClass("white-background");
+        $(this).addClass("white-background");
         $("#project-display").fadeOut(400, function () {
             $("#project-display").attr("src", "images/tuscany-full.png")
         })
         .fadeIn(400);
     });
     $("#wardrobe").on("click", function () {
+        $($(".projectButtons").children("img")).removeClass("white-background");
+        $(this).addClass("white-background");
         $("#project-display").fadeOut(400, function () {
             $("#project-display").attr("src", "images/wardrobe-full.png")
         })
         .fadeIn(400);
     });
+    /*
+    //make buttons in project row bigger on hover
+    $(".project-buttons .row .col-sm-4").hover(function () {
+        $(this.parent).toggleClass("")
+    }
+    , function () {
+
+    });
+       
+    });
+    */
+    /*
+    $(window).on("resize", function () {
+        if ($(window).width() <= 480) {
+            $(".email").toggleClass("spaced-text");
+        }
+        if ($(window).width() >= 640) {
+            $(".email").toggleClass("spaced-text");
+        }
+    });
+    */
 });
