339290c63584813cf104bc9dad6f3b5052b221eb
Joseph Coppola <jbcoppola@gmail.com>
2017-03-04 16:00:27 -0500

    condensed javascript file, reduced size of header title and changed email button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
 
     //when the nav button is clicked, it scrolls to that section
-    $(".btn-section-nav, a").on("click", function (event) {
+    $(".btn-section-nav, ul a").on("click", function (event) {
 
         //stops the href jump from occuring when javascript runs
         event.preventDefault();
@@ -11,53 +11,22 @@
             scrollTop: $($(this).attr('href')).offset().top
         },
             "slow");
-    })
-    //functions for replacing the project image with the full example picture
-    //white-background doesn't seem to currently do anything
-    $("#siegenut").on("click", function () {
-        //fade out, replace source, fade in
-        $($(".projectButtons").children("img")).removeClass("white-background");
-        $("#siegenut").addClass("white-background");
-        $("#project-display").fadeOut(400, function () {
-            $(this).attr("src", "images/siegenut-full.png");
-        })
-        .fadeIn(400);
     });
-    $("#tuscany").on("click", function () {
-        $($(".projectButtons").children("img")).removeClass("white-background");
-        $(this).addClass("white-background");
-        $("#project-display").fadeOut(400, function () {
-            $(this).attr("src", "images/tuscany-full.png")
-        })
-        .fadeIn(400);
-    });
-    $("#wardrobe").on("click", function () {
-        $($(".projectButtons").children("img")).removeClass("white-background");
-        $(this).addClass("white-background");
-        $("#project-display").fadeOut(400, function () {
-            $(this).attr("src", "images/wardrobe-full.png")
-        })
-        .fadeIn(400);
-    });
-    /*
-    //make buttons in project row bigger on hover
-    $(".project-buttons .row .col-sm-4").hover(function () {
-        $(this.parent).toggleClass("")
-    }
-    , function () {
 
-    });
-       
-    });
-    */
-    /*
-    $(window).on("resize", function () {
-        if ($(window).width() <= 480) {
-            $(".email").toggleClass("spaced-text");
-        }
-        if ($(window).width() >= 640) {
-            $(".email").toggleClass("spaced-text");
-        }
-    });
-    */
+    //project section buttons
+    $("#siegenut").on("click", function () { clickProject(this, "images/siegenut-full.png") });
+    $("#tuscany").on("click", function () { clickProject(this, "images/tuscany-full.png") });
+    $("#wardrobe").on("click", function () { clickProject(this, "images/wardrobe-full.png") });
 });
+
+//function for replacing the project image with the full example picture
+function clickProject(clicked, newImage) {
+    //white-background doesn't currently do anything
+    $($(".projectButtons").children("img")).removeClass("no-padding");
+    $(clicked).addClass("no-padding");
+    //fade out, replace source, fade in
+    $("#project-display").fadeOut(400, function () {
+        $(this).attr("src", newImage);
+    })
+    .fadeIn(400);
+}
