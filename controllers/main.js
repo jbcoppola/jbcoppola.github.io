441e04d2ad33f6826a1ca15aaade915ee8876ce8
Joe <jbcoppola@gmail.com>
2017-09-13 18:20:44 -0700

    changed spacing in sections

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('portfolio', []);
+﻿var app = angular.module('portfolio', ['ngAnimate']);
 
 app.controller('main', function ($scope, $location, $anchorScroll) {
     $scope.display = 'blank';
