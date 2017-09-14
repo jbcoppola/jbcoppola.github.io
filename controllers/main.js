ef0bfe5dd1b6fdf5cb4ed47d42802bba1e3b7aeb
Joe <jbcoppola@gmail.com>
2017-09-14 10:20:48 -0700

    made skills section borders look like brackets, used ng-repeat for skill listings

--- controllers/main.js
+++ controllers/main.js
@@ -2,4 +2,24 @@
 
 app.controller('main', function ($scope, $location, $anchorScroll) {
     $scope.display = 'blank';
+
+
+    $scope.skillList = [
+        {
+            "glyphicon": "glyphicon-sort",
+            "skills": ["Front-end Development", "Back-end Development", "ASP.NET"]
+        },
+        {
+            "glyphicon": "glyphicon-copyright-mark",
+            "skills": ["C#", "C++", "CT"]
+        },
+        {
+            "glyphicon": "glyphicon-duplicate",
+            "skills": ["Graphic Design", "Adobe Illustrator", "Adobe Photoshop"]
+        },
+        {
+            "glyphicon": "glyphicon-console",
+            "skills": ["HTML / CSS / Bootstrap", "Javascript / Jquery", "Angular"]
+        }
+    ];
 });
