c625a4f6e5724fb8a9a6f0f1147ac7cedf29429a
Joe <jbcoppola@gmail.com>
2017-09-14 10:25:41 -0700

    fixed typo

--- controllers/main.js
+++ controllers/main.js
@@ -11,7 +11,7 @@ app.controller('main', function ($scope, $location, $anchorScroll) {
         },
         {
             "glyphicon": "glyphicon-copyright-mark",
-            "skills": ["C#", "C++", "CT"]
+            "skills": ["C#", "C++", "C"]
         },
         {
             "glyphicon": "glyphicon-duplicate",
