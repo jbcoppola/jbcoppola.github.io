bc0039f991e8cd5b452f802dc681bc33977025bd
Joe <jbcoppola@gmail.com>
2017-10-17 08:28:04 -0700

    fixed company lister links

--- controllers/main.js
+++ controllers/main.js
@@ -23,8 +23,8 @@ app.controller('main', function ($scope, $location, $anchorScroll, $filter) {
             "name": "companylister",
             "thumb": "companyLister",
             "img": "companylister-full",
-            "link": "https://jbcoppola.github.io/InsitenCode/",
-            "github": "https://github.com/jbcoppola/InsitenCode",
+            "link": "https://jbcoppola.github.io/CompanyLister/",
+            "github": "https://github.com/jbcoppola/CompanyLister",
             "description": "Angular website with UI-bootstrap and non-persistent data. Enter companies into a list and view entries in either card or list UI view. Complete CRUD functionality. Display how many items are on one page at a time using pagination."
         }
     ];
