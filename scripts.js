fb7cc7cd19bc5b9481158e3699582d162b45fdd4
Joe <jbcoppola@gmail.com>
2017-10-21 22:12:14 -0700

    replaced bootstrap with custom CSS

--- scripts.js
+++ scripts.js
@@ -1,15 +1,32 @@
 ﻿$(document).ready(function () {
-
+   
     //when the nav button is clicked, it scrolls to that section
-    $(".btn-section-nav, ul a").on("click", function (event) {
+    $(".btn-section-nav, ul a").click(function (event) {
 
         //stops the href jump from occuring when javascript runs
         event.preventDefault();
 
         //gets the href location and scrolls to it
         $("body, html").animate({
-            scrollTop: $($(this).attr('href')).offset().top - 49
+            scrollTop: $($(this).attr('href')).offset().top - 55
         },
             "slow");
     });
+    navbarHide();
+    $(window).resize(function () {
+        navbarHide();
+    });
+
+    $(".hamburger").click(function () {
+        $(".navbar-items").toggleClass("hidden");
+    })
 });
+
+function navbarHide() {
+    if ($(window).width() < 544) {
+        $(".navbar-items").addClass("hidden");
+    }
+    else {
+        $(".navbar-items").removeClass("hidden");
+    }
+}
